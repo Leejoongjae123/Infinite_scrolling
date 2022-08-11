e34e6d926b4d83245f7e788c838e24859b418e00
LEE JOONG JAE <ljj3347@naver.com>
2022-08-11 11:10:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import InfiniteScroll from 'react-infinite-scroll-component';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 function App() {
+  const [iteration,setIteration]=useState(1)
+  const [items,setItems]=useState([]);
+  const [noMore,setNoMore]=useState(true)
+  let fetchItems= async ()=>{
+    let {data}= await axios.get('https://jsonplaceholder.typicode.com/posts')
+    data=data.slice(0,10)
+    setItems(data)
+  }
+  
+  useEffect(()=>{
+    fetchItems()
+  },[])
+
+  let fetchData=async()=>{
+    let {data}=await axios.get('https://jsonplaceholder.typicode.com/posts')
+    data=data.slice(10*iteration,10*(iteration+1))
+    setItems([...items,...data])
+    setIteration((prev)=>prev+1)
+    if (iteration>=10){
+      setNoMore(false)
+    }
+  }
+
+  console.log("items_array:",items)
+  console.log("iteration:",iteration)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <InfiniteScroll
+  dataLength={items.length} //This is important field to render the next data
+  next={fetchData}
+  hasMore={noMore}
+  loader={<h4>Loading...</h4>}
+  endMessage={
+    <p style={{ textAlign: 'center' }}>
+      <b>Yay! You have seen it all</b>
+    </p>
+  }
+  >
+  {items.map((elem,index)=>{
+    return(
+      <div key={index}className='container'>
+        <h4>id:{elem.id}</h4>
+        <p>title:{elem.title}</p>
+      </div>
+      
+    )
+  })}
+  </InfiniteScroll>
   );
 }
 
